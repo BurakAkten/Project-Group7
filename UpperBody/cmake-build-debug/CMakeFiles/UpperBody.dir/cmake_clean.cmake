2317ebe77acadf869235d84508eced8d880d8f4b
James Msuya <jmsuya93@gmail.com>
2018-05-03 00:16:12 +0300

    upperbody v2.0
    upperbody detector return a ector of deteted upper bodies in a frame

---
+++ UpperBody/cmake-build-debug/CMakeFiles/UpperBody.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UpperBody.dir/main.cpp.obj"
+  "UpperBody.pdb"
+  "UpperBody.exe"
+  "UpperBody.exe.manifest"
+  "libUpperBody.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UpperBody.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
